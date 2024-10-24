69ae37c7c96f975f15017c306ab3b7c4356e9914
ps4444 <47189951+ps4444@users.noreply.github.com>
2024-10-24 12:51:32 +0530

    pushing groovy (jenkins)

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,12 @@
+node {
+    stage('Build') {
+        sh 'make'
+    }
+    stage('Test') {
+        sh 'make check'
+        junit 'reports/**/*.xml'
+    }
+    stage('Deploy') {
+        sh 'make publish'
+    }
+}
