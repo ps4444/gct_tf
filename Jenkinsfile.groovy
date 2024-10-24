df65f970981080ab40abf768f22b2680e4556082
ps4444 <47189951+ps4444@users.noreply.github.com>
2024-10-24 13:19:57 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,8 +2,11 @@ node {
     stage('Build') {
     sh 'sudo rm -rf ps4444'  
     }
+    stage('Build') {
+    sh 'cd /tmp'  
+    }
         stage('Build') {
-        sh 'sudo git clone https://github.com/ps4444/gct_tf.git /ps4444'
+        sh 'sudo git clone https://github.com/ps4444/gct_tf.git ./'
     }
         stage('Build') {
         sh 'sudo cd  ps4444'  
