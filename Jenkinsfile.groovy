77dfbad6c10b8d9eaec584a458cc46b593b98b3f
ps4444 <47189951+ps4444@users.noreply.github.com>
2024-10-24 12:59:27 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,6 +2,9 @@ node {
     stage('Build') {
         sh 'terraform -fmt'
     }
+        stage('Build') {
+        sh 'git clone https://github.com/ps4444/gct_tf.git /tmp'
+    }
     stage('Test') {
         sh 'terraform -plan'
         junit 'reports/**/*.xml'
